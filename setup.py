569a65cd22753eec3a85f6daa33f6a486e35ad09
mnbf9fca <robert.aleck@cynexia.com>
2020-04-15 17:13:59 +0100

    started paclaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='EvohomeClient',
+      version='0.1',
+      description='A simple evohome client',
+      url='https://github.com/mnbf9rca/EvohomeClient',
+      author='mnbf9rca',
+      author_email='robert.aleck@cynexia.com',
+      license='MIT',
+      packages=['EvohomeClient'],
+      zip_safe=False)
