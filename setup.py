dad1efb82ec1b5fd05c11da48f5693d3a206e5f6
mnbf9fca <robert.aleck@cynexia.com>
2020-04-15 17:47:19 +0100

    packaged

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='EvohomeClient',
+setup(name='evohomeclient-mnbf9rca',
       version='0.1',
       description='A simple evohome client',
       url='https://github.com/mnbf9rca/EvohomeClient',
       author='mnbf9rca',
       author_email='robert.aleck@cynexia.com',
       license='MIT',
-      packages=['EvohomeClient'],
-      zip_safe=False)
+      packages=find_packages(),
+      install_requires=[
+          'requests',
+          'python-dotenv',
+          'python-dateutil'
+      ],
+      zip_safe=False,
+      python_requires='>=3.6')
